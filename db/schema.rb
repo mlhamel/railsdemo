ede5e174a8682ac408554e2139a1f9ff4827e6b7
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2015-07-15 03:25:19 +0000

    moves the payment type constant to his own table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710185612) do
+ActiveRecord::Schema.define(version: 20150715024834) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 20150710185612) do
     t.text     "address"
     t.string   "email"
     t.string   "pay_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "payment_types", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
