bc6d74533194f9d6ee27e9711e04d0c08a02b1fe
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2015-07-20 14:50:00 +0000

    adds user's creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715024834) do
+ActiveRecord::Schema.define(version: 20150720142247) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -53,6 +53,13 @@ ActiveRecord::Schema.define(version: 20150715024834) do
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
