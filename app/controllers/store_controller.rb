348e5d7c2c4d00c410a886d63d615821ada5f4f8
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2015-07-21 14:19:13 +0000

    adds more translation and a language switcher

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,7 +3,12 @@ class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
   def index
-    @products = Product.order(:title)
+    if params[:set_locale]
+      redirect_to store_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
+
     if session[:counter].nil?
       session[:counter] = 1
     else
--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 I18n.default_locale = :en
 
-LANGUAGUES = [
+LANGUAGES = [
   ['English',                   'en'],
-  ["Fran&ccedil;ais".html_safe, 'fr']
+  ['Fran&ccedil;ais'.html_safe, 'fr']
 ]
